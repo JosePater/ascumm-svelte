7bd6a40b385edff8c1d010b16058747f832c7780
PaterDev <josepater_1130@hotmail.com>
2023-09-12 14:34:59 -0500

    feat(header): Add us -> Mission and Vision

---
+++ src/routes/nosotros/+page.svelte
@@ -0,0 +1,32 @@
+<svelte:head>
+  <title>Nosotros</title>
+  <meta name="description" content="nosotros" />
+</svelte:head>
+
+<div class="text-column">
+  <h1>Misión</h1>
+  <p>
+    Somos una empresa con proyección regional, sólida y sostenible; producimos,
+    y comercializamos productos piscícolas de agua dulce entre los cuales
+    tenemos Bocachico, Cachama; productos agropecuarios y del campo de excelente
+    calidad e higiene, para brindar el más alto grado de satisfacción a nuestros
+    clientes y garantizar un óptimo nivel de rentabilidad para nuestra
+    Asociación.
+  </p>
+
+  <h1>Visión</h1>
+  <p>
+    Para el año 2025 nos convertiremos en una de las principales empresas
+    piscícolas y agropecuarias de la región, llevando a disposición de nuestra
+    distinguida clientela productos de excelente calidad, con valor agregado y
+    responsabilidad con el medio ambiente; generadora desarrollo y empleo para
+    nuestra asociación y la comunidad.
+  </p>
+
+</div>
+
+<style>
+    p {
+        font-size:larger;
+    }
+</style>
